89ae6b0c68fd5cffe89ad98f31ecbc5962a47c31
Neil Berg <neil.berg14@gmail.com>
2020-11-15 22:33:02 -0800

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Gohn
 
-WIP
+A simple CLI for fetching Hacker News stories.
+
+## Prerequites
+
+[Download and install Go](https://golang.org/doc/install)
+
+## Quick start
+
+Clone the repo, compile the code, and run it on your computer.
+
+```bash
+git clone https://github.com/neil-berg/gohn.git
+cd gohn
+go build gohn.go
+./gohn --count=5 // between 1 and 10 stories to fetch
+```
--- flags/flags.go
+++ flags/flags.go
@@ -6,13 +6,16 @@ import (
 
 // ParseFlags parses command line arguments to fetch Hacker News stories
 func ParseFlags() *int {
-	defaultCount := 5
-	countPtr := flag.Int("count", defaultCount, "Number of stories to fetch")
+	countPtr := flag.Int("count", 5, "Number of stories to fetch between 1 and 10")
 
 	flag.Parse()
 
-	if *countPtr < 0 {
-		*countPtr = defaultCount
+	if *countPtr < 1 {
+		*countPtr = 1
+	}
+
+	if *countPtr > 10 {
+		*countPtr = 10
 	}
 
 	return countPtr
